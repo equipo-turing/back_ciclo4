4c2f6b0e80eedc4ef80ff6c46b614ca9b234bc30
Rojas-Andres <andresfelipe200004@gmail.com>
2021-11-25 00:04:40 -0500

    Añadiendo usuario y proyecto

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import {conectarBD} from './db/db.js';
+
 import dotenv from 'dotenv';
-import {AvancesModel} from './model/avances.js';
-import {objetivoModel} from './model/objetivo.js';
+import conectarBD from './db/db.js';
+import { UserModel } from './models/usuario/usuario.js';
+import { ProjectModel } from './models/proyecto/proyecto.js';
+import { ObjectiveModel } from './models/objective.js';
+//import { Enum_EstadoUsuario, Enum_Rol, Enum_TipoObjetivo } from './models/enums/enums';
 
 dotenv.config();//para que funcione el archivo .env
 
@@ -15,7 +18,6 @@ const main=async()=>{
         descripcion:'primer avance',
         observaciones:'el primer avance fué exitoso',
        // creadopor:"id mongo del usuario"
-
     }).then((avances)=>{
         console.log('coleccion avances creada: ',avances);
     }).catch((e)=>{
@@ -24,15 +26,31 @@ const main=async()=>{
  */
     // colección objetivo
 
-    await objetivoModel.create({
+    /*await ObjectiveModel.create({
         descripcion:"permitir generar los objetivos ",
-        
-       
     }).then((objetivo)=>{
         console.log('coleccion objetivo creado',objetivo);
     }).catch((e)=>{
         console.log('error  creando objetivo',e);
-    });
+    });*/
+
+    const usuarioInicial = await UserModel.create({
+        nombre: 'Andres',
+        apellido: 'Saldarriaga',
+        correo: 'andres@cc.com',
+        identificacion: '12345',
+        rol: 'LIDER',
+        estado: 'AUTORIZADO',
+      });
+    
+      const proyectoCreado = await ProjectModel.create({
+        nombre: 'UNIVERSIDAD ANTIOQUIA',
+        fechaInicio: new Date('2021/12/24'),
+        fechaFin: new Date('2022/12/24'),
+        presupuesto: 120000,
+        lider: usuarioInicial._id,
+      });
+    
 }
 
 main();
