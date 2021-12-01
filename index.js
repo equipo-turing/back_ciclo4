c25d0564134849f823686bff2aa5515d4ad985f2
Rojas-Andres <andresfelipe200004@gmail.com>
2021-12-01 16:37:21 -0500

    graphql

--- index.js
+++ index.js
@@ -1,66 +1,32 @@
-
+import express from 'express';
+import cors from 'cors';
+import {ApolloServer} from 'apollo-server-express';
 import dotenv from 'dotenv';
-import conectarBD from './db/db.js';
-import { UserModel } from './models/usuario/usuario.js';
-import { ProjectModel } from './models/proyecto/proyecto.js';
-import { ObjectiveModel } from './models/objective.js';
-//import { Enum_EstadoUsuario, Enum_Rol, Enum_TipoObjetivo } from './models/enums/enums';
+import connectDB from './db/db.js';
+import { tipos} from './graphql/types.js';
+import { resolvers } from './graphql/resolvers.js';
 
-import { InscriptionModel } from './models/inscripcion/inscripcion.js';
 
-dotenv.config();//para que funcione el archivo .env
+dotenv.config();
 
-const main=async()=>{
-    await conectarBD();//me conecto a la base de datos
-    
-    //creo la colección (tabla) "avances" en mongodb
-     /* await AvancesModel.create({ 
-         //le paso datos quemados a la colección:      
-        fecha:new Date("05-1-2021"),
-        descripcion:'primer avance',
-        observaciones:'el primer avance fué exitoso',
-       // creadopor:"id mongo del usuario"
-    }).then((avances)=>{
-        console.log('coleccion avances creada: ',avances);
-    }).catch((e)=>{
-        console.error("error creando coleción avances ",e);
-    }); 
- */
-    // colección objetivo
+const server = new ApolloServer({
+  typeDefs: tipos, 
+  resolvers: resolvers,
 
-    /*await ObjectiveModel.create({
-        descripcion:"permitir generar los objetivos ",
-    }).then((objetivo)=>{
-        console.log('coleccion objetivo creado',objetivo);
-    }).catch((e)=>{
-        console.log('error  creando objetivo',e);
-    });*/
+});
 
-    const usuarioInicial = await UserModel.create({
-        nombre: 'LAURA',
-        apellido: 'Trujillo',
-        correo: 'laura@unilibre.com',
-        identificacion: '31231',
-        rol: 'ESTUDIANTE',
-        estado: 'AUTORIZADO',
-      });
-    
-    const proyectoCreado = await ProjectModel.create({
-        nombre: 'UNIVERSIDAD LIBRE',
-        fechaInicio: new Date('2021/12/24'),
-        fechaFin: new Date('2022/12/24'),
-        presupuesto: 111111,
-        lider: usuarioInicial._id,
-    });
-    
-    //falta crear la inscripcion
-    const inscripcionCreada = await InscriptionModel.create({
-        estado:'ACEPTADO',
-        fechaIngreso: new Date('2021/12/24'),
-        fechaEgreso: new Date('2022/12/24'),
-        proyecto:proyectoCreado.id,
-        estudiante:usuarioInicial._id,
-    });
-}
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-main();
+app.listen({ port: process.env.PORT || 4000 },async ()=>{
+  await connectDB();
+  await server.start();
+
+  server.applyMiddleware({app});
+
+  console.log('servidor listo');
+});
+
+
+
